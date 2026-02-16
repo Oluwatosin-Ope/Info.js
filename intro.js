51ce48f985f7ce613c9ac71b26c5e6a604f2ec0c
Aileru Oluwatosin <ailerut55@gmail.com>
2026-02-16 09:47:59 +0100

    Added madlib Game

--- intro.js
+++ intro.js
@@ -1,3 +1,9 @@
 console.log("Name: Aileru Tosin");
 console.log("Age: 23");
 console.log("Favorite Programming Language: JavaScript");
+const colour = "red"
+const place = "churches"
+const celeb = "Simi"
+console.log("Roses are", colour); 
+console.log(place, "are blue");
+console.log("I love", celeb)
